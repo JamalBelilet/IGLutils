5e88b6fba55bb5eed30247e72338e3452e7a54c8
jamal blt <df_belilet@esi.dz>
2017-10-13 22:13:24 +0100

    init IGL utils project

---
+++ app.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by bjama on 10/13/2017.
+ */
+
+
+class VectorHelper {
+    constructor(private vector) {}
+    sortVector() {}
+    addVectors() {}
+    inverseVector() {}
+    getMaxMin() {}
+    formula() {}
+}
