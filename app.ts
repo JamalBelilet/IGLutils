2f44e8f7d915c60ce2b64ce3d6eca56f87e6c588
moumed <moumed2810@gmail.com>
2017-10-16 00:12:19 +0100

    toutes les fonctions sont retablits reste juste le min et le max

--- app.ts
+++ app.ts
@@ -1,21 +1,21 @@
 var vector1 = function(boxes)
-    {
-        v1 =[];
-        for (var i = 1 ; i <= boxes; i++) {
-            var value = prompt("insert the value "+i);
-                v1.push(value);
-        }
-        console.log(v1);
-        alert('vector created ');
-        return vector1;
+{
+    v1 =[];
+    for (var i = 1 ; i <= boxes; i++) {
+        var value = prompt("insert the value "+i);
+        v1.push(value);
     }
+    console.log(v1);
+    alert('vector created ');
+    return vector1;
+}
 
 var vector2 = function(boxes)
 {
     v2 =[];
     for (var i = 1 ; i <= boxes; i++) {
         var value = prompt("insert the value "+i);
-        v1.push(value);
+        v2.push(value);
     }
     console.log(v2);
     alert('vector created ');
@@ -23,15 +23,25 @@ var vector2 = function(boxes)
 }
 
 var list = function (v)
-    {
-        console.log(v);
-        alert('vector listed');
-    }
+{
+    console.log(v);
+    alert('vector listed');
+    return v;
+}
 
 var sort = function (v) {
-        v.sort();
-        console.log(v);
-        alert('vector sorted');
+    for (var i = 0; i < v.length; i++) {
+        for (var j = i + 1; j < v.length; j++) {
+            if (v[i] > v[j]) {
+                var swap = v[i];
+                v[i] = v[j];
+                v[j] = swap;
+            }
+        }
+    }
+    console.log(v);
+    alert('vector sorted');
+    return v;
 }
 
 var sum = function (v1,v2)
@@ -40,42 +50,68 @@ var sum = function (v1,v2)
     {
         throw (alert('must be the same length !!'))
     }
-        else
+    else
+    {
+        var v3 = [];
+        for(var i=0 ; i<=v1.length-1 ; ++i)
         {
-            v3 = [];
-            for(var i=0 ; i<=v1.length ; i++)
-            {
-                v3[i]=v1[i]+v2[i];
-            }
-            console.log(v3);
-            alert('two vectors have been summed in v3');
+            v3[i]=Number(v1[i])+Number(v2[i]);
         }
+
+        console.log(v3);
+        alert('two vectors have been summed in v3');
+    }
+    return v3;
 }
-var reverse = function (v) {
-    v.reverse();
+var reverse = function (v)
+{
+    var j=0;
+    var swap;
+    for (var i = (v.length-1) ; i>(Math.floor(v.length/2)); --i )
+    {
+        swap = v[j];
+        v[j]=v[i];
+        v[i]=swap;
+    }
     console.log(v);
+    alert('vector is reversed');
+    return v;
 }
 var v1,v2 =[];
-    while (option !='quit')
+while (option !='quit')
+{
+    var  option = prompt('select an option');
+    if (option === "create")
     {
-        var  option = prompt('select an option');
-        if (option === "create")
-        {
-            var boxes = prompt("insert the number of boxes");
-            vector1(Number(boxes));
-        }
-        else if(option ==="list")
-        {
-            var name = prompt("insert the name of the vector v1 or v2");
-            if (name === "v1"){list(v1)}
-            else if (name === "v2"){list(v2)}
-            else {alert('name does not exist')};
-        }
-        else if(option ==="sort")
-        {
-            var name = prompt("insert the name of the vector v1 or v2");
-            if (name === "v1"){sort(v1)}
-            else if (name === "v2"){sort(v2)}
-            else {alert('name does not exist')};
-        }
+        var boxes = prompt("insert the number of boxes");
+        vector1(Number(boxes));
     }
+    else if(option ==="list")
+    {
+        var name = prompt("insert the name of the vector v1 or v2");
+        if (name === "v1"){list(v1)}
+        else if (name === "v2"){list(v2)}
+        else {alert('name does not exist')};
+    }
+    else if(option ==="sort")
+    {
+        var name = prompt("insert the name of the vector v1 or v2");
+        if (name === "v1"){sort(v1)}
+        else if (name === "v2"){sort(v2)}
+        else {alert('name does not exist')};
+    }
+    else if(option ==="summon")
+    {
+
+        var boxes = prompt("insert the number of boxes of the seconde vector v2");
+        vector2(Number(boxes));
+        sum(v1,v2);
+    }
+    else if(option ==="reverse")
+    {
+        var name = prompt("insert the name of the vector v1 or v2");
+        if (name === "v1"){reverse(v1)}
+        else if (name === "v2"){reverse(v2)}
+        else {alert('name does not exist')};
+    }
+}
